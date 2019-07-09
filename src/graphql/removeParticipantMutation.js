5a2b34f5c6f0e31deb0add425ba4e8c44c730f1b
Herman Ng <heymanhn@gmail.com>
2019-07-08 18:05:24 -0700

    Update the DELETE endpoint per backend spec

--- src/graphql/removeParticipantMutation.js
+++ src/graphql/removeParticipantMutation.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export default gql`
   mutation RemoveParticipant($id: String!) {
-    removeParticipant(id: $id, userId: $userId) @rest(type: "Success", path: "/meetings/{args.id}/access/{args.userId}", method: "DELETE") {
+    removeParticipant(id: $id, userId: $userId) @rest(type: "Success", path: "/meetings/{args.id}/access?user_id={args.userId}", method: "DELETE") {
       success
     }
   }
