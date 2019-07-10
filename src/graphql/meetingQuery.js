57c200a47624989c0a19ac3b1fa6bc9251a951a8
recmend <arun@airseed.com>
2019-07-10 15:41:28 -0700

    show due date

--- src/graphql/meetingQuery.js
+++ src/graphql/meetingQuery.js
@@ -4,6 +4,7 @@ export default gql`
   query Meeting($id: String!) {
     meeting(id: $id) @rest(type: "Meeting", path: "/meetings/{args.id}") {
       id
+      deadline
       title
       author @type(name: "User") {
         id
