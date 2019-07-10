b575b40fa3851c3d635de9bcf8d8de644e189863
recmend <arun@airseed.com>
2019-07-10 15:07:49 -0700

    filter child level conversations

--- src/graphql/meetingConversationsQuery.js
+++ src/graphql/meetingConversationsQuery.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export default gql`
   query MeetingConversations($id: String!) {
-    conversationsQuery(id: $id) @rest(type: "Conversations", path: "/meetings/{args.id}/conversations", method: "GET") {
+    conversationsQuery(id: $id) @rest(type: "Conversations", path: "/meetings/{args.id}/conversations?exclude_child_level=true", method: "GET") {
       conversations @type(name: "[Conversation]") {
         id
         createdAt
