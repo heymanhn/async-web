30d6e993733099d613fda74b8fc0b02347a701fa
Herman Ng <heymanhn@gmail.com>
2019-07-10 10:27:43 -0700

    isOpen states on the notifications button

--- src/components/notifications/NotificationSystem.jsx
+++ src/components/notifications/NotificationSystem.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBell as solidBell } from '@fortawesome/free-solid-svg-icons';
 import { faBell as regularBell } from '@fortawesome/free-regular-svg-icons';
@@ -6,12 +7,42 @@ import styled from '@emotion/styled';
 
 const StyledIcon = styled(FontAwesomeIcon)(({ theme: { colors } }) => ({
   color: colors.grey3,
+  cursor: 'pointer',
   fontSize: '20px',
   margin: '0 10px',
 }));
 
-const NotificationSystem = () => (
-  <StyledIcon icon={regularBell} />
-);
+class NotificationSystem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+    };
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  toggleDropdown(event) {
+    event.stopPropagation();
+
+    this.setState(prevState => ({ isOpen: !prevState.isOpen }));
+    // TODO: Mark notifications as read
+  }
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <StyledIcon
+        icon={isOpen ? solidBell : regularBell}
+        onMouseDown={this.toggleDropdown}
+      />
+    );
+  }
+}
+
+NotificationSystem.propTypes = {
+};
 
 export default NotificationSystem;
