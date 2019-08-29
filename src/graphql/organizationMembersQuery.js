b8fc5ee05ff347a40a5afb683a358ec3b194b772
recmend <arun@airseed.com>
2019-08-29 15:00:53 -0400

    fix typo

--- src/graphql/organizationMembersQuery.js
+++ src/graphql/organizationMembersQuery.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export default gql`
   query OrganizationMembers($id: String!) {
-    organizationMembers(id: $id) @rest(type: "[User]", path: "/organizations/{args.Id}/members", method: "GET") {
+    organizationMembers(id: $id) @rest(type: "[User]", path: "/organizations/{args.id}/members", method: "GET") {
       id
       fullName
       profilePictureUrl
