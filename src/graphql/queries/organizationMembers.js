ae8f93d04d9ea295aeb20e0395d4ef60980184c5
Herman Ng <heymanhn@gmail.com>
2019-08-30 22:09:02 -0700

    Move graphql query to new path

--- src/graphql/queries/organizationMembers.js
+++ src/graphql/queries/organizationMembers.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag';
 
 export default gql`
   query OrganizationMembers($id: String!) {
-    organizationMembers(id: $id) @rest(type: "[User]", path: "/organizations/{args.id}/members", method: "GET") {
+    organizationMembers(id: $id) @rest(type: "OrganizationMembers", path: "/organizations/{args.id}/members", method: "GET") {
       members @type(name: "[User]") {
         user @type(name: "User") {
           id
