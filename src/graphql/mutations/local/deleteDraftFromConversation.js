677ae8f89c65d6d16244d0748150f9f9a4f8eaf7
Herman Ng <heymanhn@gmail.com>
2019-10-29 22:43:46 -0700

    Fix bug with discarding drafts

--- src/graphql/mutations/local/deleteDraftFromConversation.js
+++ src/graphql/mutations/local/deleteDraftFromConversation.js
@@ -2,6 +2,6 @@ import gql from 'graphql-tag';
 
 export default gql`
   mutation DeleteDraftFromConversation($conversationId: String!) {
-    DeleteDraftFromConversation(conversationId: $conversationId) @client
+    deleteDraftFromConversation(conversationId: $conversationId) @client
   }
 `;
