ef28db7669cbdc7e5ced348761d7c4fa959f89e7
Herman Ng <heymanhn@gmail.com>
2019-11-01 19:09:22 -0700

    Simple dropdown menu for logout and invite people (#81)

--- src/components/navigation/Sidebar.jsx
+++ src/components/navigation/Sidebar.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { navigate } from '@reach/router';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
 // hwillson forgot to export useLazyQuery to the react-apollo 3.0 release
@@ -10,7 +9,7 @@ import organizationQuery from 'graphql/queries/organization';
 import { getLocalAppState } from 'utils/auth';
 
 import Avatar from 'components/shared/Avatar';
-import Button from 'components/shared/Button';
+import DropdownMenu from './DropdownMenu';
 import MeetingSpacesList from './MeetingSpacesList';
 import SwitchToButton from './SwitchToButton';
 
@@ -27,7 +26,14 @@ const Container = styled.div(({ theme: { colors, sidebarWidth } }) => ({
   zIndex: 1,
 }));
 
+const MenuContainer = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  position: 'relative',
+});
+
 const OrganizationDisplay = styled.div({
+  cursor: 'pointer',
   display: 'flex',
   flexDirection: 'row',
 
@@ -44,23 +50,11 @@ const OrganizationTitle = styled.div(({ theme: { colors } }) => ({
   fontWeight: 500,
 }));
 
-// Temporary UI
-const SignOutButtonContainer = styled.div(({ theme: { colors } }) => ({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'space-between',
-
-  background: colors.darkBlue,
-  position: 'sticky',
-  bottom: '0px',
-  padding: '20px',
-}));
-
-const StyledButton = styled(Button)({
-  padding: '3px 15px',
-});
-
 const Sidebar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  function openDropdown() { setIsDropdownOpen(true); }
+  function closeDropdown() { setIsDropdownOpen(false); }
+
   const { organizationId } = getLocalAppState();
   const [getOrganization, { loading, data }] = useLazyQuery(organizationQuery, {
     variables: { id: organizationId },
@@ -77,33 +71,27 @@ const Sidebar = () => {
 
   return (
     <Container>
-      <OrganizationDisplay>
-        <OrganizationAvatar
-          avatarUrl={logo}
-          size={24}
-          square
-        />
-        <OrganizationTitle>{title}</OrganizationTitle>
-      </OrganizationDisplay>
+      <MenuContainer>
+        <OrganizationDisplay>
+          <OrganizationAvatar
+            avatarUrl={logo}
+            onClick={openDropdown}
+            size={24}
+            square
+          />
+          <OrganizationTitle onClick={openDropdown}>{title}</OrganizationTitle>
+        </OrganizationDisplay>
 
+        <DropdownMenu
+          handleClose={closeDropdown}
+          isOpen={isDropdownOpen}
+          organizationId={organizationId}
+        />
+      </MenuContainer>
       {/* Placeholder button row for now */}
       <SwitchToButton />
 
       <MeetingSpacesList />
-
-      {/* NOTE: Temporary buttons here */}
-      <SignOutButtonContainer>
-        <StyledButton
-          onClick={() => navigate(`/organizations/${organizationId}/invites`)}
-          title="Invite people"
-          type="grey"
-        />
-        <StyledButton
-          onClick={() => navigate('/logout')}
-          title="Sign out"
-          type="grey"
-        />
-      </SignOutButtonContainer>
     </Container>
   );
 };
