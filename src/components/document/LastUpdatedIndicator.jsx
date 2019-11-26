c82a01d7a80d9e6ed10eb959af4397d828fa3304
Herman Ng <heymanhn@gmail.com>
2019-11-26 01:04:17 -0800

    Support auto-saving for documents (#85)
    * Auto-save logic
    
    * Last Updated indicator UI

---
+++ src/components/document/LastUpdatedIndicator.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+import styled from '@emotion/styled';
+
+const REFRESH_INTERVAL = 15000;
+
+const Container = styled.div(({ theme: { colors } }) => ({
+  display: 'flex',
+  flexDirection: 'row',
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  background: colors.white,
+  color: colors.grey4,
+  cursor: 'default',
+  fontSize: '14px',
+  height: '60px',
+  padding: '0 30px',
+  opacity: 0.95,
+  position: 'fixed',
+  bottom: 0,
+  right: 0,
+}));
+
+const Label = styled.div({
+  fontWeight: 500,
+});
+
+const Timestamp = styled(Moment)({
+  marginLeft: '4px',
+  marginTop: '2px',
+});
+
+const LastUpdatedIndicator = ({ timestamp }) => (
+  <Container>
+    <Label>Updated</Label>
+    <Timestamp interval={REFRESH_INTERVAL} fromNow>{timestamp}</Timestamp>
+  </Container>
+);
+
+LastUpdatedIndicator.propTypes = {
+  timestamp: PropTypes.number.isRequired,
+};
+
+export default LastUpdatedIndicator;
