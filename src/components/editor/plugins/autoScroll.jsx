acc40d2ebd1b58e4f7a0d4fd1e93206f8c00799c
Herman Ng <heymanhn@gmail.com>
2019-12-02 14:06:14 -0800

    One more autoscroll fix

--- src/components/editor/plugins/autoScroll.jsx
+++ src/components/editor/plugins/autoScroll.jsx
@@ -5,6 +5,8 @@ const MINIMUM_DISTANCE_FROM_BOTTOM = 60;
 function AutoScroll() {
   function updateWindowPosition() {
     const native = window.getSelection();
+    if (native && native.rangeCount === 0) return;
+
     const range = native.getRangeAt(0);
     const rect = range.getBoundingClientRect();
 
