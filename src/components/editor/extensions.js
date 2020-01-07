670b5445562c37f83559b528c29c1485d0df04a0
Herman Ng <heymanhn@gmail.com>
2020-01-06 17:32:58 -0800

    Uncomment code

--- src/components/editor/extensions.js
+++ src/components/editor/extensions.js
@@ -228,7 +228,7 @@ const discussionPlugins = [
 const documentPlugins = [
   ...coreEditorPlugins,
 
-  // AutoSave(),
+  AutoSave(),
   AutoScroll(),
   CompositionMenu(),
   InlineDiscussionPlugin(),
