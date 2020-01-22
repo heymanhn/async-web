8d930c5bcbd9dec61cbdc7a782995b8c46c33dbb
recmend <arun@airseed.com>
2020-01-22 11:00:10 -0800

    redirect to organization's default document instead of empty page

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -3,17 +3,22 @@ import { useQuery } from 'react-apollo';
 import { Redirect } from '@reach/router';
 
 import localStateQuery from 'graphql/queries/localState';
+import organizationQuery from 'graphql/queries/organization';
 import { getLocalAppState } from 'utils/auth';
 
+import Document from 'components/document/Document';
 import LoggedOutHome from './LoggedOutHome';
 
 const Home = () => {
   const { data: localStateData } = useQuery(localStateQuery);
-
-  if (!localStateData) return null;
   const { isLoggedIn, isOnboarding } = localStateData;
   const { organizationId } = getLocalAppState();
+  const { loading, data } = useQuery(organizationQuery, {
+    variables: { id: organizationId },
+  });
 
+  if (!localStateData) return null;
+  if (loading) return null;
   if (!isLoggedIn) return <LoggedOutHome />;
 
   if (isOnboarding) {
@@ -21,7 +26,9 @@ const Home = () => {
     return <Redirect to={path} noThrow />;
   }
 
-  return <div>Hi, you are logged in</div>;
+  const { defaultDocumentId } = data.organization;
+
+  return <Document documentId={defaultDocumentId} />;
 };
 
 export default Home;
