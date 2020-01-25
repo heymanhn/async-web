690c55a579f56cd485d0a4a3a146479ef3f101f3
recmend <arun@airseed.com>
2020-01-24 19:03:47 -0800

    fix lint error

--- src/components/document/DiscussionListItem.jsx
+++ src/components/document/DiscussionListItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useQuery } from 'react-apollo';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
