e793e85eecafd656872ef83488e1f273f8d94d2b
recmend <arun@airseed.com>
2020-02-06 15:02:10 +0700

    abstracted discussion mutations

--- src/graphql/mutations/createDiscussion.ts
+++ src/graphql/mutations/createDiscussion.ts
@@ -4,8 +4,8 @@ import discussion from 'graphql/fragments/discussion';
 import message from 'graphql/fragments/message';
 
 export default gql`
-  mutation CreateDocumentDiscussion($documentId: String!, $input: Object!) {
-    createDocumentDiscussion(documentId: $documentId, input: $input) @rest(type: "Discussion", path: "/documents/{args.documentId}/discussions", method: "POST") {
+  mutation CreateDiscussion($input: Object!) {
+    createDiscussion(input: $input) @rest(type: "Discussion", path: "/discussions", method: "POST") {
       ...DiscussionObject
       messages @type(name: "Message") {
         ...MessageObject
