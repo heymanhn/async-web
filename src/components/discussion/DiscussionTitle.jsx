9f8b5654fa3a7ffa9101b3359e28167bd8920f6e
recmend <arun@airseed.com>
2020-02-07 10:31:26 +0700

    refactor NavTitle into Document and Discussion Title for better code restructuring

---
+++ src/components/discussion/DiscussionTitle.jsx
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import styled from '@emotion/styled';
+import { useQuery } from '@apollo/react-hooks';
+
+import { DiscussionContext } from 'utils/contexts';
+import discussionQuery from 'graphql/queries/discussion';
+
+const Title = styled.div(({ theme: { colors } }) => ({
+  color: colors.grey1,
+  fontSize: '14px',
+  position: 'relative',
+}));
+
+const DiscussionTitle = () => {
+  const { discussionId } = useContext(DiscussionContext);
+
+  const { loading, data } = useQuery(discussionQuery, {
+    variables: { discussionId, queryParams: {} },
+  });
+
+  if (loading || !data.discussion) return null;
+  const { discussion } = data;
+  const { topic } = discussion;
+  const { text: title } = topic || {};
+
+  return <Title>{title || 'Untitled'}</Title>;
+};
+
+export default DiscussionTitle;
