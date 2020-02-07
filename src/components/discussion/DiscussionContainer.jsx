55b650260937506ea28551ff9a048069fce672ca
recmend <arun@airseed.com>
2020-02-07 13:05:27 +0700

    more styling changes for the Discussion page

--- src/components/discussion/DiscussionContainer.jsx
+++ src/components/discussion/DiscussionContainer.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
 
 import { DiscussionContext } from 'utils/contexts';
 
 import HeaderBar from 'components/navigation/HeaderBar';
 import Discussion from './Discussion';
+
+const Content = styled.div(({ theme: { colors } }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+
+  // Vertically center the page when content doesn't fit full height
+  minHeight: 'calc(100vh - 60px)', // 60px for the navigation bar
+  margin: '0 auto',
+  background: colors.bgGrey,
+}));
 
 const DiscussionContainer = ({ discussionId }) => {
   const value = {
@@ -14,7 +25,9 @@ const DiscussionContainer = ({ discussionId }) => {
   return (
     <DiscussionContext.Provider value={value}>
       <HeaderBar />
-      <Discussion />
+      <Content>
+        <Discussion />
+      </Content>
     </DiscussionContext.Provider>
   );
 };
