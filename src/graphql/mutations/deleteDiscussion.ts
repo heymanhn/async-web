6e12f5ea07bb406f6c7db3a284e0cf6e240947d6
recmend <arun@airseed.com>
2020-02-11 10:32:31 +0700

    update deleteDiscussionMutation to not rely on now documentId optional field

--- src/graphql/mutations/deleteDiscussion.ts
+++ src/graphql/mutations/deleteDiscussion.ts
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag';
 
 export default gql`
-  mutation DeleteDiscussion($documentId: String!, $discussionId: String!) {
-    deleteDiscussion(documentId: $documentId, discussionId: $discussionId) @rest(type: "DeleteDiscussionResponse", path: "/documents/{args.documentId}/discussions/{args.discussionId}", method: "DELETE") {
+  mutation DeleteDiscussion($discussionId: String!) {
+    deleteDiscussion(discussionId: $discussionId) @rest(type: "DeleteDiscussionResponse", path: "/discussions/{args.discussionId}", method: "DELETE") {
       success
     }
   }
