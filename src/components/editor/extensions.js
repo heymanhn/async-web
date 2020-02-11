4fe243e40ba534b7618a4369b2511052a6ce7501
Herman Ng <heymanhn@gmail.com>
2020-02-10 16:27:00 -0800

    Fix bad merge and clean up unused code

--- src/components/editor/extensions.js
+++ src/components/editor/extensions.js
@@ -1,28 +1,11 @@
 import PlaceholderPlugin from 'slate-react-placeholder';
-import AutoReplace from 'slate-auto-replace';
 import SoftBreak from 'slate-soft-break';
 import styled from '@emotion/styled';
 
 import { theme } from 'styles/theme';
-import { track } from 'utils/analytics';
 
 import { DEFAULT_NODE, DEFAULT_PLAIN_NODE } from './utils';
-<<<<<<< HEAD
-import { BoldPlugin } from './plugins/marks/bold';
-import { ItalicPlugin } from './plugins/marks/italic';
-import Underlined from './plugins/marks/underlined';
-import CodeHighlight from './plugins/marks/codeHighlight';
-import { TextBlockPlugin } from './plugins/blocks/text';
-=======
->>>>>>> WIP: Get composition menu working
-import { HeadingsPlugin } from './plugins/blocks/headings';
-import { SectionBreak } from './plugins/blocks/sectionBreak';
-import { Image } from './plugins/blocks/image';
-import { ListsPlugin } from './plugins/blocks/lists';
 import Link from './plugins/inlines/link';
-import { BlockQuotePlugin } from './plugins/blocks/blockQuote';
-import { CodeBlockPlugin } from './plugins/blocks/codeBlock';
-import CompositionMenu from './plugins/compositionMenu';
 
 // HN: Not supporting drag and drop guides for now
 // import DragAndDrop from './plugins/dragAndDrop';
@@ -38,45 +21,6 @@ import {
 /* **** Commands **** */
 
 export const commands = {
-  clearBlock: editor => {
-    if (editor.isEmptyBlock()) return null;
-    return editor
-      .moveAnchorToStartOfBlock()
-      .moveFocusToEndOfBlock()
-      .delete();
-  },
-
-  setWrappedBlock: (editor, type, source) => {
-    // This means the user is looking to un-set the wrapped block
-    if (editor.isWrappedBy(type)) {
-      return editor.setBlocks(DEFAULT_NODE).unwrapBlock(type);
-    }
-
-    track('Block inserted to content', { type, source });
-
-    return editor
-      .setBlocks(DEFAULT_NODE)
-      .unwrapAnyBlock(type)
-      .wrapBlock(type);
-  },
-
-  setBlock: (editor, type, source) => {
-    const isActive = editor.hasBlock(type);
-
-    // We're not interested in text blocks...
-    if (!isActive && type !== DEFAULT_NODE)
-      track('Block inserted to content', { type, source });
-
-    return editor.unwrapAnyBlock().setBlocks(isActive ? DEFAULT_NODE : type);
-  },
-
-  unwrapAnyBlock: (editor, type) => {
-    const parent = editor.getParentBlock();
-    if (!parent || parent.type === type) return;
-
-    editor.unwrapBlock(parent.type);
-  },
-
   moveEndToStartOfParentBlock: (editor, start) => {
     const { value } = editor;
     const { document } = value;
@@ -105,15 +49,6 @@ export const commands = {
 /* **** Queries  **** */
 
 export const queries = {
-  getParentBlock: editor => {
-    const { document, startBlock } = editor.value;
-    if (!startBlock) return null;
-    return document.getClosestBlock(startBlock.key);
-  },
-  hasBlock: (editor, type) =>
-    editor.value.blocks.some(node => node.type === type),
-  hasActiveMark: (editor, type) =>
-    editor.value.activeMarks.some(mark => mark.type === type),
   isAtBeginning: editor => {
     const { value } = editor;
     const { selection } = value;
@@ -139,10 +74,6 @@ export const queries = {
     const { selection } = value;
 
     return editor.isEmptyDocument() && !selection.isFocused;
-  },
-  isEmptyParagraph: editor => {
-    const { startBlock } = editor.value;
-    return startBlock && startBlock.type === DEFAULT_NODE && !startBlock.text;
   },
   isWrappedBy: (editor, type) =>
     editor.value.blocks.some(
@@ -180,39 +111,19 @@ const createDocumentPlaceholder = text =>
   });
 
 const coreEditorPlugins = [
-  // Blocks
-  HeadingsPlugin(),
-  ListsPlugin(),
-  BlockQuotePlugin(),
-  CodeBlockPlugin(),
-  SectionBreak(),
-  Image(),
-
   // Inlines
   Link(),
 
   // Others
   SoftBreak({ shift: true }),
-  AutoReplace({
-    trigger: 'space',
-    before: /(--)$/,
-    change: change => change.insertText('— '),
-  }),
   ImageLoadingIndicator(),
 ];
 
 const documentPlugins = [
   ...coreEditorPlugins,
   AutoScroll(),
-  CompositionMenu(),
   InlineDiscussionPlugin(),
   createDocumentPlaceholder('Say what you have to say'),
-];
-
-const inlineDiscussionPlugins = [
-  ...coreEditorPlugins,
-
-  CompositionMenu({ isModal: true }),
 ];
 
 const contextPlugins = [...coreEditorPlugins, ContextHighlightPlugin()];
@@ -220,6 +131,5 @@ const contextPlugins = [...coreEditorPlugins, ContextHighlightPlugin()];
 export const plugins = {
   documentTitle: [createDocumentTitlePlaceholder('Untitled Document')],
   document: documentPlugins,
-  discussionMessage: inlineDiscussionPlugins,
   discussionContext: contextPlugins,
 };
