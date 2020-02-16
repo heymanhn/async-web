27ceb4ba8ddcb9ea482fa0222915b7961ce0757f
Herman Ng <heymanhn@gmail.com>
2020-02-16 12:44:39 -0800

    Don't activate key handlers for readonly composers

--- src/components/editor/plugins/inlineDiscussion.jsx
+++ src/components/editor/plugins/inlineDiscussion.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
 import InlineDiscussion from 'components/discussion/InlineDiscussion';
-import { RenderInline, RenderMark } from './helpers';
+import { RenderMark } from './helpers';
 
 export const INLINE_DISCUSSION_ANNOTATION = 'inline-discussion';
 export const CONTEXT_HIGHLIGHT = 'context-highlight';
@@ -27,21 +26,4 @@ export function InlineDiscussionPlugin() {
   }
 
   return [RenderMark(INLINE_DISCUSSION_ANNOTATION, renderInlineDiscussion)];
-}
-
-const ContextHighlight = styled.span(({ theme: { colors } }) => ({
-  background: colors.highlightYellow,
-  padding: '2px 0px',
-}));
-
-export function ContextHighlightPlugin() {
-  function renderContextHighlight(props) {
-    const { attributes, children } = props; /* eslint react/prop-types: 0 */
-
-    return (
-      <ContextHighlight attributes={attributes}>{children}</ContextHighlight>
-    );
-  }
-
-  return [RenderInline(CONTEXT_HIGHLIGHT, renderContextHighlight)];
 }
--- src/components/editor/useCoreEditorProps.jsx
+++ src/components/editor/useCoreEditorProps.jsx
@@ -4,11 +4,13 @@ import { triggerMarkHotkeys, triggerBlockHotkeys } from './hotkeys';
 import Leaf from './Leaf';
 import Element from './Element';
 
-const useCoreEditorProps = editor => {
+const useCoreEditorProps = (editor, { readOnly } = {}) => {
   const renderLeaf = useCallback(props => <Leaf {...props} />, []);
   const renderElement = useCallback(props => <Element {...props} />, []);
 
   function onKeyDown(event) {
+    if (readOnly) return;
+
     triggerMarkHotkeys(editor, event);
     triggerBlockHotkeys(editor, event);
   }
