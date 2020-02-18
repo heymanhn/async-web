657bf2cb16b834472a9319ec7bf567f0e4f44ea4
recmend <arun@airseed.com>
2020-02-18 12:53:21 +0700

    Adding pagination support

--- src/components/homepage/Inbox.jsx
+++ src/components/homepage/Inbox.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useQuery } from 'react-apollo';
 import styled from '@emotion/styled';
 
 import { getLocalUser } from 'utils/auth';
 import inboxQuery from 'graphql/queries/inbox';
+import useInfiniteScroll from 'utils/hooks/useInfiniteScroll';
+import { snakedQueryParams } from 'utils/queryParams';
+
 import NewDocumentButton from 'components/document/NewDocumentButton';
 import NewDiscussionButton from 'components/discussion/NewDiscussionButton';
-
 import InboxRow from './InboxRow';
 import NavBar from './Navbar';
 import InboxViewMode from './InboxViewMode';
@@ -36,20 +38,60 @@ const ButtonContainer = styled.div({
 
 const Inbox = () => {
   const [viewMode, setViewMode] = useState('all');
+  const inboxRef = useRef(null);
+  const [shouldFetch, setShouldFetch] = useInfiniteScroll(inboxRef);
+  const [isFetching, setIsFetching] = useState(false);
 
   const { userId } = getLocalUser();
-  const { loading, error, data } = useQuery(inboxQuery, {
+  const { loading, error, data, fetchMore } = useQuery(inboxQuery, {
     variables: { id: userId, queryParams: { type: viewMode } },
   });
   if (loading) return null;
   if (error || !data.inbox) return <Container>Error fetching page</Container>;
 
-  const { items } = data.inbox;
+  const { items, pageToken, totalHits } = data.inbox;
+
+  const fetchMoreMessages = () => {
+    const newQueryParams = { type: viewMode };
+    if (pageToken) newQueryParams.pageToken = pageToken;
+
+    fetchMore({
+      query: inboxQuery,
+      variables: {
+        id: userId,
+        queryParams: snakedQueryParams(newQueryParams),
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const { items: previousItems } = previousResult.inbox;
+        const {
+          items: newItems,
+          pageToken: newToken,
+          totalHits: newHits,
+        } = fetchMoreResult.inbox;
+        setShouldFetch(false);
+        setIsFetching(false);
+
+        return {
+          inbox: {
+            items: [...previousItems, ...newItems],
+            pageToken: newToken,
+            totalHits: totalHits + newHits,
+            __typename: fetchMoreResult.inbox.__typename,
+          },
+        };
+      },
+    });
+  };
+
+  if (shouldFetch && pageToken && !isFetching) {
+    setIsFetching(true);
+    fetchMoreMessages();
+  }
 
   return (
     <Container>
       <NavBar />
-      <InnerContainer>
+      <InnerContainer ref={inboxRef}>
         <PageTitle>Inbox</PageTitle>
         <InboxViewMode viewMode={viewMode} setViewMode={setViewMode} />
         {items.map(item => {
