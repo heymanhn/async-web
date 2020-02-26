9fff1e77afa666d081092355aba1d692e5a967fe
recmend <arun@airseed.com>
2020-02-26 14:52:56 +0700

    DocumentDiscussion query schema changes to avoid updating discussion object cache

--- src/graphql/fragments/discussionItems.ts
+++ src/graphql/fragments/discussionItems.ts
@@ -1,18 +1,12 @@
 import gql from 'graphql-tag';
 
-import discussion from 'graphql/fragments/discussion';
-import messageContext from 'graphql/fragments/messageContext';
-
 export default gql`
   fragment DiscussionItems on DiscussionsResponse {
     items @type(name: "[DiscussionItem]") {
       discussion @type(name: "Discussion") {
-        ...DiscussionObject
-        ...MessageContext
+        id
       }
     }
     pageToken
   }
-  ${discussion}
-  ${messageContext}
 `;
