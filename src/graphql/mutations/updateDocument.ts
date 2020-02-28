abc8d3dd3fd9868fb23bd0ac917845e8be6bb916
Herman Ng <heymanhn@gmail.com>
2020-02-28 15:51:22 -0800

    Update document mutation response to document object
    Backend returns the full document object. We can expect it in the
    response so that Apollo/GraphQL knows to update the object in
    cache. Avoids a refetchQueries call.

--- src/graphql/mutations/updateDocument.ts
+++ src/graphql/mutations/updateDocument.ts
@@ -1,9 +1,17 @@
 import gql from 'graphql-tag';
 
+import doc from 'graphql/fragments/document';
+
 export default gql`
   mutation UpdateDocument($documentId: String!, $input: Object!) {
-    updateDocument(documentId: $documentId, input: $input) @rest(type: "Document", path: "/documents/{args.documentId}", method: "PUT") {
-      id
+    updateDocument(documentId: $documentId, input: $input)
+      @rest(
+        type: "Document"
+        path: "/documents/{args.documentId}"
+        method: "PUT"
+      ) {
+      ...DocumentObject
     }
   }
+  ${doc}
 `;
