2579bac961850497898289fc8a650a3e4f1910b0
Herman Ng <heymanhn@gmail.com>
2020-02-28 14:59:12 -0800

    Simplify content state hook
    Let's not use them for plaintext titles

--- src/utils/hooks/useContentState.jsx
+++ src/utils/hooks/useContentState.jsx
@@ -1,34 +1,25 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import {
-  DEFAULT_ELEMENT,
-  deserializeString,
-  toPlainText,
-} from 'components/editor/utils';
+import { DEFAULT_ELEMENT } from 'components/editor/utils';
 
 /*
  * Manages the state for a Slate composer component.
  *
  * Updates the state value if the initial content has changed.
  */
-const useContentState = (initialContent, { isJSON = true } = {}) => {
-  const formatContent = useCallback(
-    c => (isJSON ? JSON.parse(c) : deserializeString(c)),
-    [isJSON]
-  );
-
+const useContentState = initialContent => {
   const [content, setContent] = useState(
-    initialContent ? formatContent(initialContent) : DEFAULT_ELEMENT
+    initialContent ? JSON.parse(initialContent) : DEFAULT_ELEMENT
   );
 
   useEffect(() => {
-    if (initialContent) setContent(formatContent(initialContent));
-  }, [initialContent, formatContent]);
+    setContent(initialContent ? JSON.parse(initialContent) : DEFAULT_ELEMENT);
+  }, [initialContent]);
 
   return {
-    content: isJSON ? content : toPlainText(content),
+    content,
 
-    // For spreading into <Slate /> provider components
+    // For spreading into input elements, such as <Slate /> provider components
     value: content,
     onChange: c => setContent(c),
   };
