fef0625615dbfdd97711393c4872a417908829b7
Herman Ng <heymanhn@gmail.com>
2020-02-29 02:36:22 -0800

    Cmd + Shift + D to start inline discussion with selection
    Only works if text is selected

--- src/components/editor/toolbar/InlineDiscussionButton.jsx
+++ src/components/editor/toolbar/InlineDiscussionButton.jsx
@@ -1,12 +1,15 @@
 import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { Transforms } from 'slate';
+import { Transforms, Range } from 'slate';
 import { useSlate } from 'slate-react';
 
 import { DocumentContext } from 'utils/contexts';
+import useKeyDownHandler from 'utils/hooks/useKeyDownHandler';
 
 import ToolbarButton from 'components/editor/toolbar/ToolbarButton';
 import ButtonIcon from 'components/editor/toolbar/ButtonIcon';
+
+const INLINE_DISCUSSION_HOTKEY = 'cmd+shift+d';
 
 const InlineDiscussionButton = ({ content, ...props }) => {
   const editor = useSlate();
@@ -19,6 +22,10 @@ const InlineDiscussionButton = ({ content, ...props }) => {
 
     Transforms.deselect(editor);
   };
+
+  const { selection } = editor;
+  const isOpen = selection && Range.isExpanded(selection);
+  useKeyDownHandler([INLINE_DISCUSSION_HOTKEY, handleClick], !isOpen);
 
   return (
     <ToolbarButton handleClick={handleClick} {...props}>
