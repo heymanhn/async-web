232e93d6d8c09ce764c8339790148f45bc745eec
Herman Ng <heymanhn@gmail.com>
2020-03-02 19:51:07 -0800

    Remove unused code

--- src/utils/hooks/usePaginatedResource.jsx
+++ src/utils/hooks/usePaginatedResource.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useQuery } from 'react-apollo';
 
 import { snakedQueryParams } from 'utils/queryParams';
