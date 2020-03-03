12da6c9dbf74a549aa4c27132b4413057ee556ed
Herman Ng <heymanhn@gmail.com>
2020-03-02 16:06:10 -0800

    Track dimensions as state in the hook to improve performance

--- src/utils/hooks/useSelectionDimensions.jsx
+++ src/utils/hooks/useSelectionDimensions.jsx
@@ -8,35 +8,23 @@
  *   provider component
  * - DiscussionContext provided with a reference to the modal component, if
  *   a modal is currently displayed
- *
- * Consumer requirements (TODO):
- * - Needs to return updated dimensions upon window resize or editor change
- *  (e.g. user inserts/removes text or makes a selection)
  */
 import { useContext, useState } from 'react';
+import { ReactEditor, useSlate } from 'slate-react';
 
 import useMountEffect from 'utils/hooks/useMountEffect';
 import { DiscussionContext } from 'utils/contexts';
 
-const useSelectionDimensions = () => {
+const useSelectionDimensions = skip => {
   const { modalRef } = useContext(DiscussionContext);
-  const [recalculate, setRecalculate] = useState(false);
+  const [data, setData] = useState({});
+  const editor = useSlate();
 
-  useMountEffect(() => {
-    const handleResize = () => setRecalculate(true);
-
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  });
-
-  // Not using Slate's helpers to get the DOM range because sometimes the
-  // DOM node for a Slate node hasn't been created yet at the time of
-  // this calculation
   const calculateDimensions = () => {
-    const native = window.getSelection();
-    const range = native.getRangeAt(0);
+    const { selection } = editor;
+    if (!selection || skip) return;
+
+    const range = ReactEditor.toDOMRange(editor, selection);
     const rect = range.getBoundingClientRect();
     const { height, width } = rect;
 
@@ -45,20 +33,43 @@ const useSelectionDimensions = () => {
     const yOffset = modal ? modal.scrollTop : window.pageYOffset;
     const xOffset = modal ? modal.scrollLeft : window.pageXOffset;
 
-    return {
-      coords: {
-        top: rect.top + yOffset,
-        left: rect.left + xOffset,
-      },
-      dimensions: {
-        height,
-        width,
-      },
+    const coords = {
+      top: rect.top + yOffset,
+      left: rect.left + xOffset,
     };
+
+    const dimensions = {
+      height,
+      width,
+    };
+
+    const { coords: oldCoords, dimensions: oldDimensions } = data;
+    if (
+      oldCoords &&
+      oldDimensions &&
+      oldCoords.top === coords.top &&
+      oldCoords.left === coords.left &&
+      oldDimensions.height === dimensions.height &&
+      oldDimensions.width === dimensions.width
+    )
+      return;
+
+    setData({ coords, dimensions });
   };
 
-  if (recalculate) setRecalculate(false);
-  return calculateDimensions();
+  useMountEffect(() => {
+    window.addEventListener('resize', calculateDimensions);
+    return () => {
+      window.removeEventListener('resize', calculateDimensions);
+    };
+  });
+
+  // Needed so that the DOM has a chance to:
+  // 1. Render any new nodes and ranges for text insertions
+  // 2. Update the selection range so that getBoundingClientRect() is correct
+  setTimeout(calculateDimensions, 0);
+
+  return data;
 };
 
 export default useSelectionDimensions;
