6c8c8ba7e81383ba69778128da4585848efc52e4
Herman Ng <heymanhn@gmail.com>
2020-04-14 14:42:35 -0700

    Look for full reaction object in POST /reactions response

--- src/graphql/mutations/createReaction.ts
+++ src/graphql/mutations/createReaction.ts
@@ -1,9 +1,13 @@
 import gql from 'graphql-tag';
 
+import reaction from 'graphql/fragments/reaction';
+
 export default gql`
   mutation CreateReaction($input: Object!) {
-    createReaction(input: $input) @rest(type: "Reaction", path: "/reactions", method: "POST") {
-      success
+    createReaction(input: $input)
+      @rest(type: "Reaction", path: "/reactions", method: "POST") {
+      ...ReactionObject
     }
   }
+  ${reaction}
 `;
