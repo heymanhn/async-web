74f2c1750215d13e95cdde4de0a00ea85103b70f
Herman Ng <heymanhn@gmail.com>
2020-04-15 01:58:13 -0700

    WIP: Get badge count updating to work

--- src/utils/hooks/useViewedReaction.jsx
+++ src/utils/hooks/useViewedReaction.jsx
@@ -1,21 +1,25 @@
-import { useApolloClient, useMutation, useQuery } from '@apollo/react-hooks';
+import { useMutation, useQuery } from '@apollo/react-hooks';
 import Pluralize from 'pluralize';
 
 import createReactionMutation from 'graphql/mutations/createReaction';
 import localUpdateBadgeCountMtn from 'graphql/mutations/local/updateBadgeCount';
 import localMarkResourceAsReadMtn from 'graphql/mutations/local/markResourceAsRead';
 import localMarkNotificationAsReadMtn from 'graphql/mutations/local/markNotificationAsRead';
-import resourceNotificationsQuery from 'graphql/queries/resourceNotifications';
 import discussionQuery from 'graphql/queries/discussion';
 import documentQuery from 'graphql/queries/document';
-import localMarkWorkspaceResourceAsRead from 'graphql/mutations/local/markWorkspaceResourceAsRead';
-import { getLocalUser } from 'utils/auth';
+
 import useDisambiguatedResource from 'utils/hooks/useDisambiguatedResource';
 
 const useViewedReaction = () => {
-  const client = useApolloClient(); // TODO (HN): Check if this is still needed
   const resource = useDisambiguatedResource();
-  const { resourceType, resourceId } = resource;
+  const { resourceType, resourceId, resourceQuery, variables } = resource;
+
+  // Clever way to create a query function that returns a Promise ;-)
+  // https://github.com/apollographql/react-apollo/issues/3499#issuecomment-586039082
+  const { refetch: fetchResource } = useQuery(resourceQuery, {
+    variables,
+    skip: true,
+  });
 
   const [createReaction] = useMutation(createReactionMutation, {
     variables: {
@@ -36,104 +40,30 @@ const useViewedReaction = () => {
     variables: { incrementBy: -1 },
   });
 
-  // TODO (HN): use a useLazyQuery to fetch the resource later?
-
-  // const getParentResourceId = (resourceType, resourceId) => {
-  //   let discussion;
-  //   let document;
-  //   if (resourceType === 'discussion') {
-  //     const data = client.readQuery({
-  //       query: discussionQuery,
-  //       variables: { discussionId: resourceId },
-  //     });
-  //     discussion = data && data.discussion;
-  //   } else if (resourceType === 'document') {
-  //     const data = client.readQuery({
-  //       query: documentQuery,
-  //       variables: { documentId: resourceId },
-  //     });
-  //     document = data && data.document;
-  //   }
-
-  //   const resource = discussion || document;
-  //   if (!resource) return {};
-  //   const { workspaces } = resource;
-  //   const documentId =
-  //     (document && document.id) || (discussion && discussion.documentId);
-  //   const workspaceId = workspaces ? workspaces[0] : undefined;
-
-  //   return { documentId, workspaceId };
-  // };
-
   // const { userId } = getLocalUser();
-  // const { documentId, workspaceId } = getParentResourceId(
-  //   resourceType,
-  //   resourceId
-  // );
-
-  // let refetchQueries = [
-  //     {
-  //       query: resourceNotificationsQuery,
-  //       variables: {
-  //         resourceType: 'users',
-  //         resourceId: userId,
-  //         queryParams: {},
-  //       },
-  //     },
-  //   ];
-  //   [
-  //     { resourceType: 'workspace', resourceId: workspaceId },
-  //     { resourceType: 'document', resourceId: documentId },
-  //   ].forEach(item => {
-  //     if (item.resourceId) {
-  //       refetchQueries = [
-  //         ...refetchQueries,
-  //         {
-  //           query: resourceNotificationsQuery,
-  //           variables: {
-  //             resourceType: Pluralize(item.resourceType),
-  //             resourceId: item.resourceId,
-  //             queryParams: {},
-  //           },
-  //         },
-  //       ];
-  //     }
-  //   });
-
-  // HN: See if this is needed
-  const markWorkspaceResourceAsRead = () => {
-    //     let notificationResourceId = resourceId;
-    //     // TODO: think of a better to handle multiple local mututations.
-    //     if (documentId) notificationResourceId = documentId;
-    //
-    //     // If this resource is part of a workspace, mark the state as Read
-    //     if (workspaceId) {
-    //       client.mutate({
-    //         mutation: localMarkWorkspaceResourceAsRead,
-    //         variables: {
-    //           workspaceId,
-    //           resourceType,
-    //           resourceId: notificationResourceId,
-    //         },
-    //       });
-    //     }
-
-    return console.log('TODO: Mark workspace resource as read...');
-  };
 
   // Updates the sidebar ResourceRow badgeCounts
   // 1. If a discussion or document, decrement badge count for given resource in sidebar
   // 2. If workspace, or if discussion/document has a parent workspace, decrement badge count
   //    for the workspace in the sidebar
   // 3. If inline discussion, decrement badge count for the parent document
-  const decrementResourceBadgeCounts = () => {
+  const decrementResourceBadgeCounts = async () => {
+    const { data } = await fetchResource();
+
+    if (!data[resourceType])
+      return Promise.reject(new Error('Unable to fetch resource'));
+
+    const { documentId: parentDocumentId, workspaces } = data[resourceType];
+    const parentWorkspaceId = workspaces ? workspaces[0] : undefined;
+
     // localUpdateBadgeCount({
     //   variables: {
     //     resourceType: workspaceId ? 'workspace' : resourceType,
     //     resourceId: workspaceId || notificationResourceId,
     //   },
     // });
-    return console.log('TODO: Decrement resource badge counts...');
+
+    return Promise.resolve();
   };
 
   const markAsRead = async () => {
@@ -143,9 +73,10 @@ const useViewedReaction = () => {
       const { createReaction: reaction } = data;
 
       localMarkResourceAsRead({ variables: { reaction } });
-      localMarkNotifAsRead();
-      markWorkspaceResourceAsRead();
       decrementResourceBadgeCounts();
+
+      // This will also mark the workspace resource as read
+      localMarkNotifAsRead();
 
       return Promise.resolve();
     }
