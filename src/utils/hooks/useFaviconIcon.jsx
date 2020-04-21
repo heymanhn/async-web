0e673f426b1480744a96d6f7fda0be42aec096ea
Herman Ng <heymanhn@gmail.com>
2020-04-20 18:54:18 -0700

    Update favicon in useEffect() hook
    This prevents multiple renders of all the components, due to useQuery.

--- src/utils/hooks/useFaviconIcon.jsx
+++ src/utils/hooks/useFaviconIcon.jsx
@@ -1,29 +1,37 @@
+import { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import resourceNotificationsQuery from 'graphql/queries/resourceNotifications';
 import { getLocalUser } from 'utils/auth';
 
 const useFaviconIcon = () => {
-  const favicon = window.document.getElementById('favicon');
   const { userId } = getLocalUser();
 
-  const { data } = useQuery(resourceNotificationsQuery, {
+  const { refetch: getNotifications } = useQuery(resourceNotificationsQuery, {
     variables: { resourceType: 'users', resourceId: userId, queryParams: {} },
-    skip: !userId,
+    skip: true,
   });
 
-  if (!data || !data.resourceNotifications) return null;
-  const { items } = data.resourceNotifications;
-  if (!items) return null;
+  useEffect(() => {
+    const favicon = window.document.getElementById('favicon');
 
-  const unreadNotifications = (items || []).filter(n => n.readAt < 0);
-  if (unreadNotifications.length > 0) {
-    favicon.href = '/favicon-new.png';
-  } else {
-    favicon.href = '/favicon.png';
-  }
+    const handleFetch = async () => {
+      if (!userId) return;
 
-  return null;
+      const { data } = await getNotifications();
+      const { items } = data.resourceNotifications;
+      if (items) {
+        const unreadNotifications = (items || []).filter(n => n.readAt < 0);
+        if (unreadNotifications.length > 0) {
+          favicon.href = '/favicon-new.png';
+        } else {
+          favicon.href = '/favicon.png';
+        }
+      }
+    };
+
+    handleFetch();
+  }, [getNotifications, userId]);
 };
 
 export default useFaviconIcon;
