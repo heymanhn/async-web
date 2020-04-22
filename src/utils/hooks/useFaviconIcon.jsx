0c1836af70585be1f9df0a331b153435dcf4b33d
Herman Ng <heymanhn@gmail.com>
2020-04-21 17:30:59 -0700

    Undo useEffect() based implementation of the favicon hook
    Can't avoid rendering this hook two times, since we need to observe
    the resourceNotifications query and update favicon whenever that data
    changes.
    
    In general, there's no strict requirement that top level components
    are only allowed to render once. We should not make these assumptions
    in any of the child components' code.

--- src/utils/hooks/useFaviconIcon.jsx
+++ src/utils/hooks/useFaviconIcon.jsx
@@ -1,10 +1,10 @@
-import { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 
 import resourceNotificationsQuery from 'graphql/queries/resourceNotifications';
 import { getLocalUser } from 'utils/auth';
 
 const useFaviconIcon = () => {
+  const favicon = window.document.getElementById('favicon');
   const { userId } = getLocalUser();
 
   const { data } = useQuery(resourceNotificationsQuery, {
@@ -12,20 +12,16 @@ const useFaviconIcon = () => {
     skip: !userId,
   });
 
-  useEffect(() => {
-    const favicon = window.document.getElementById('favicon');
-
-    if (!userId || !data || !data.resourceNotifications) return;
-    const { items } = data.resourceNotifications;
-    if (items) {
-      const unreadNotifications = (items || []).filter(n => n.readAt < 0);
-      if (unreadNotifications.length > 0) {
-        favicon.href = '/favicon-new.png';
-      } else {
-        favicon.href = '/favicon.png';
-      }
+  if (!userId || !data || !data.resourceNotifications) return;
+  const { items } = data.resourceNotifications;
+  if (items) {
+    const unreadNotifications = (items || []).filter(n => n.readAt < 0);
+    if (unreadNotifications.length > 0) {
+      favicon.href = '/favicon-new.png';
+    } else {
+      favicon.href = '/favicon.png';
     }
-  }, [data, userId]);
+  }
 };
 
 export default useFaviconIcon;
