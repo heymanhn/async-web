18f67d60973fb3f8df3c76216e982455e6ac2b93
Herman Ng <heymanhn@gmail.com>
2020-05-05 11:22:19 -0700

    z-index fix for Reaction picker tooltip

--- src/components/message/ReactionPicker.jsx
+++ src/components/message/ReactionPicker.jsx
@@ -21,7 +21,7 @@ const Container = styled.div(({ isOpen, offset, theme: { colors } }) => ({
   padding: '10px 5px 5px',
   position: 'absolute',
   transition: 'opacity 0.2s',
-  zIndex: 1,
+  zIndex: 4,
 }));
 
 const Title = styled.div(({ theme: { colors } }) => ({
