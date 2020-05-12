4cb95191e0a4a0e868c7b6f4e687163a0842a4e1
Herman Ng <heymanhn@gmail.com>
2020-05-12 12:12:28 -0700

    Logic for trimming whitespace from beginning and end of content

--- src/hooks/message/useMessageMutations.jsx
+++ src/hooks/message/useMessageMutations.jsx
@@ -12,6 +12,7 @@ import useDiscussionMutations from 'hooks/discussion/useDiscussionMutations';
 import { track } from 'utils/analytics';
 import { DocumentContext, MessageContext } from 'utils/contexts';
 import { toPlainText } from 'utils/editor/constants';
+import trimContent from 'utils/editor/helpers';
 
 const useMessageMutations = ({ message = null } = {}) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -60,14 +61,15 @@ const useMessageMutations = ({ message = null } = {}) => {
       });
     }
 
+    const trimmedMessage = trimContent(message);
     const { data } = await createMessage({
       variables: {
         discussionId: messageDiscussionId,
         input: {
           body: {
             formatter: 'slatejs',
-            text: toPlainText(message),
-            payload: JSON.stringify(message),
+            text: toPlainText(trimmedMessage),
+            payload: JSON.stringify(trimmedMessage),
           },
         },
       },
