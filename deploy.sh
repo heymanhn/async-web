7b1bfae58ad1f11dbbf60c4a1ca7856ac5fae117
Herman Ng <heymanhn@gmail.com>
2020-05-13 14:21:59 -0700

    Update local deployment shell script
    In case we ever need to trigger a deploy locally, at least make
    sure the commands are identical to the CircleCI deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Deploys to S3 & Cloudfront
+# Local deploytment script - deploys to S3 & Cloudfront
 
 aws s3 sync build/ s3://async-web --cache-control max-age=172800 --delete
+aws s3 cp s3://async-web/index.html s3://async-web/index.html --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths /index.html
